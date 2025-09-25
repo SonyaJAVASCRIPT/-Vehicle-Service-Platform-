8d1a8424491696e8ac74f45f0b72cd7ad3b11617
SonyaJAVASCRIPT <claireroush13iq@gmail.com>
2025-09-25 15:54:55 +0200

    feat(user-service, prisma): add database connection

--- backend/user-service/src/app.controller.ts
+++ backend/user-service/src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
-
+import { AppService } from './app.service';
+type User = {
+  username: string;
+  email: string;
+};
 @Controller()
 export class AppController {
+  constructor(private appService: AppService) {}
   @EventPattern('USER_CREATED')
-  handleUserCreated(@Payload() data: any) {
-    console.log('Data recieved: ', data);
+  async handleUserCreated(@Payload() data: User) {
+    await this.appService.createUser(data.username, data.email);
   }
 }
--- backend/user-service/src/app.service.ts
+++ backend/user-service/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+  async createUser(username: string, email: string) {
+    return this.prisma.user.create({
+      data: { username: username, email: email },
+    });
+  }
+  async getUsers() {
+    return await this.prisma.user.findMany();
   }
 }
